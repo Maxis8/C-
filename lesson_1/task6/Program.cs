b0893c1ccc8cf929550ceb1187b4115a075c657b
Max <imv-1max@yandex.ru>
2022-08-23 22:13:37 +0500

    add task6

---
+++ lesson_1/task6/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+double result = num / 2;
+if (num == result * 2) Console.Write("Да");
+else Console.Write("Нет");
