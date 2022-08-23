1b7f9376bdb978ea95a2b2d1cb78dacae094a10a
Max <imv-1max@yandex.ru>
2022-08-23 19:53:57 +0500

    add task4

---
+++ lesson_1/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int c = int.Parse(Console.ReadLine());
+
+if (b < a && a > c)
+{
+    Console.Write(a);
+}
+else if (a < b && b > c)
+{
+    Console.Write(b);
+}
+else
+{
+    Console.Write(c);
+}
+
+
