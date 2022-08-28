757ab000b24b88fcff5061683e041b420251e77e
Max <imv-1max@yandex.ru>
2022-08-29 00:09:41 +0500

    lesson2

---
+++ lesson_2/task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру (справа налево) заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int a = num % 1000;
+int a1 = a;
+a1 = a / 100;
+  
+     if (a1 == 0)
+     {   
+         Console.WriteLine($"{num} третьей цифры не существует");
+     }
+     else 
+     {
+        Console.WriteLine($"{num}, {a1} ");
+     }
---
+++ lesson_2/task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число: ");
+int days = int.Parse(Console.ReadLine());
+
+
+  
+     if (days > 0 && days < 6 )
+     {   
+         Console.WriteLine("Нет");
+     }
+     else if (days > 5 && days < 8) 
+     {
+        Console.WriteLine("Да");
+     }
+     else
+     {
+        Console.WriteLine("Такого дня не существует");
+     }
