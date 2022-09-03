b0ee1a8d6e1b15031224633c5b6993d301193b00
Max <imv-1max@yandex.ru>
2022-09-03 11:21:25 +0500

    lesson4 up task29

--- lesson_4/task29/Program.cs
+++ lesson_4/task29/Program.cs
@@ -1,21 +1,23 @@
 ﻿// Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами из [a,b) и выводит их на экран.
 
-
-
-int[] IntArray()
-{    
-    int[] arr = new int [12];
+int[] IntArray(int num, int min, int max)
+{   
+    int[] arr = new int [num];
     for(int i=0; i<arr.Length; i++)
     { 
-        arr[i] = new Random().Next(0, 35);
         
+        arr[i] = new Random().Next(min, max);
     }
     return arr;    
  
 }
  
 
+Console.WriteLine("Введите размер массива: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение диапазона: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение диапазона: ");
+int b = int.Parse(Console.ReadLine());
 
-
-Console.WriteLine(String.Join(", ", IntArray()));
-
+Console.WriteLine(String.Join(", ", IntArray(N , a, b)));
