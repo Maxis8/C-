58c009b2943d308ad21fc05de78fdeb5e628d74c
Max <imv-1max@yandex.ru>
2022-09-22 03:47:34 +0500

    lesson9

---
+++ lesson_9/task66/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+int SumRec(int m, int n )
+{
+    if (m==n) return m; 
+    return m += SumRec(m +1, n);
+}
+
+Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = SumRec(num1, num2);
+Console.WriteLine(sum);
---
+++ lesson_9/task68/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+Console.WriteLine("Введите m: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int num2 = int.Parse(Console.ReadLine());
+int akkernum = Akkerman(num1, num2);
+Console.Write($"Функция Аккермана = {akkernum} ");
